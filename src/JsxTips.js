2fc78e18e46416800cff937648bbe270d10dfa90
aaaaus <ausluft@gmail.com>
2019-04-12 23:28:36 -0400

    jsx tips update

--- src/JsxTips.js
+++ src/JsxTips.js
@@ -1,20 +1,31 @@
 import React from 'react';
 
+function getPlaceholderText() {
+  return 'e.g. Homer Simpson'
+}
+
 const JsxTips = () => {
 
+  //HOW JSX DIFFERS FROM HTML
+
   //see below for examples of how to inline style JSX
+
+  //className must be used in JSX rather than class (however in newer versions of React this may change)
+
+  //POWERFUL: JSX can reference JS variables
+
+  const buttonText = 'Click me!';
 
   return (
     <div style={{ border: '1px solid green', padding: '10px'}}>
       <label className="label" for="name">
         Enter name:
       </label>
-      <input id="name" type="text" />
+      <input id="name" placeholder={getPlaceholderText()} type="text" />
       <button style={{ backgroundColor: 'orange', color: 'black' }}>
-        Submit
+        {buttonText}
       </button>
     </div>
-
   )
 
 }
