125a13c88b74e045a520b79cefed1d6f4baf4a4d
aaaaus <ausluft@gmail.com>
2019-04-15 17:20:47 -0400

    seasons css file created; styling completed

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
+import './SeasonDisplay.css'; //Webpack will see this and import it to index.html
+
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: 'sun'
+  },
+  winter: {
+    text: "Brr it's cold!",
+    iconName: 'snowflake'
+  }
+}
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? 'summer' : 'winter';
@@ -11,14 +24,14 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = (props) => {
 
   const season = getSeason(props.lat, new Date().getMonth())
-  const text = season === 'summer' ? "Let's hit the beach!" : "Brr, it's chilly."
-  const iconType = season === 'summer' ? 'sun' : 'snowflake';
+
+  const { text, iconName } = seasonConfig[season]
 
   return (
-    <div>
-      <i className={`sun icon`} /> {/* interpolation tics must be in brackets */}
-      {text}
-      <i className={`sun icon`} />
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} /> {/* interpolation tics must be in brackets */}
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`} />
     </div>
   )
 }
