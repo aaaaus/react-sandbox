b2667a8385b35d4b8a756ac53b544127efcbb7d9
aaaaus <ausluft@gmail.com>
2019-04-16 19:14:34 -0400

    pics: wiring up event handler

--- src/components/PicsSearchBar.js
+++ src/components/PicsSearchBar.js
@@ -2,13 +2,24 @@ import React from 'react';
 
 class PicsSearchBar extends React.Component {
 
+  onInputChange(event) {
+    console.log(event.target.value)
+  }
+
+  //common property names for wiring up event handlers:
+  //onChange
+  //onClick
+  //onSubmit
+
+  //note below, onInputChange is NOT being invoked with () - if done this way, it will be run on render.
+  //We want to only pass reference to this function, to be used when there is a change.
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" />
+            <input type="text" onChange={this.onInputChange}/>
           </div>
         </form>
       </div>
