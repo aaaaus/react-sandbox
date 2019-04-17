a6669fe6134f34ae497c79e443abb3c5c7972d47
aaaaus <ausluft@gmail.com>
2019-04-17 16:29:17 -0400

    hidden api key, deactivated old key

--- src/components/PicsApp.js
+++ src/components/PicsApp.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import PicsSearchBar from './PicsSearchBar';
 import PicsImageList from './PicsImageList';
 
-const apiKey = process.env.REACT_APP_UNSPLASHED_API_KEY
+const apiKey = process.env.REACT_APP_UNSPLASH_API_KEY
 
 class PicsApp extends React.Component {
 
@@ -12,7 +12,6 @@ class PicsApp extends React.Component {
 
   // async onSearchSubmit(term) { //rewrite this as arrow below to bind this, note that async keyword moves
   onSearchSubmit = async (term) => {
-    debugger
     const resp = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
