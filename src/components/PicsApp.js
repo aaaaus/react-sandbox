412aa167b62ed1e4977da3c77ec5e1f8cd53ca77
aaaaus <ausluft@gmail.com>
2019-04-17 16:08:41 -0400

    cleanup

--- src/components/PicsApp.js
+++ src/components/PicsApp.js
@@ -4,16 +4,19 @@ import axios from 'axios';
 import PicsSearchBar from './PicsSearchBar';
 import PicsImageList from './PicsImageList';
 
+const apiKey = process.env.REACT_APP_UNSPLASHED_API_KEY
+
 class PicsApp extends React.Component {
 
   state = { images: [] }
 
   // async onSearchSubmit(term) { //rewrite this as arrow below to bind this, note that async keyword moves
   onSearchSubmit = async (term) => {
+    debugger
     const resp = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
-        Authorization: 'Client-ID ef4e4ee42e39a1c2d0591cf45f6901ffbc5d8c2de19e625c0f1b1aa041d662fc'
+        Authorization: `Client-ID ${apiKey}`
       }
     })
 
@@ -28,7 +31,7 @@ class PicsApp extends React.Component {
   //     .get('https://api.unsplash.com/search/photos', {
   //       params: { query: term },
   //       headers: {
-  //         Authorization: 'Client-ID ef4e4ee42e39a1c2d0591cf45f6901ffbc5d8c2de19e625c0f1b1aa041d662fc'
+  //         Authorization: 'Client-ID ${apiKey}'
   //       }
   //     })
   //     .then((resp) => {
