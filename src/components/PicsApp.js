43e0cdd903c7022b374d44e6b77e8afb376690f4
aaaaus <ausluft@gmail.com>
2019-04-17 11:10:26 -0400

    refactored axios GET request to async await syntax

--- src/components/PicsApp.js
+++ src/components/PicsApp.js
@@ -5,16 +5,32 @@ import PicsSearchBar from './PicsSearchBar';
 
 class PicsApp extends React.Component {
 
-  onSearchSubmit(term) {
-    console.log("Search term is: ", term);
-
-    axios.get('https://api.unsplash.com/search/photos', {
+  async onSearchSubmit(term) {
+    const resp = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
         Authorization: 'Client-ID ef4e4ee42e39a1c2d0591cf45f6901ffbc5d8c2de19e625c0f1b1aa041d662fc'
       }
-    });
+    })
+
+    console.log(resp.data.results);
   }
+
+  //old way of resolving promise:
+
+  // onSearchSubmit(term) {
+  //
+  //   axios
+  //     .get('https://api.unsplash.com/search/photos', {
+  //       params: { query: term },
+  //       headers: {
+  //         Authorization: 'Client-ID ef4e4ee42e39a1c2d0591cf45f6901ffbc5d8c2de19e625c0f1b1aa041d662fc'
+  //       }
+  //     })
+  //     .then((resp) => {
+  //       console.log(resp.results.data);
+  //     })
+  // }
 
   render() {
 
