0faf2e2970164dc3281de89a086e3b5898bbc2cf
aaaaus <ausluft@gmail.com>
2019-04-17 10:51:51 -0400

    axios configured for GET requests from unsplashed API

--- src/components/PicsApp.js
+++ src/components/PicsApp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from 'axios'; 
+import axios from 'axios';
 
 import PicsSearchBar from './PicsSearchBar';
 
@@ -7,6 +7,13 @@ class PicsApp extends React.Component {
 
   onSearchSubmit(term) {
     console.log("Search term is: ", term);
+
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID ef4e4ee42e39a1c2d0591cf45f6901ffbc5d8c2de19e625c0f1b1aa041d662fc'
+      }
+    });
   }
 
   render() {
